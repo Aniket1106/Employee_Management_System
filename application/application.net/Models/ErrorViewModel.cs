9900258a3ef31f17237ccc76a77ad5864a75cef0
Aman Shrivastava <amann.shrivastava@gmail.com>
2024-04-29 15:50:07 +0530

    deleted files of previous app

--- application/application.net/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace application.net.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
