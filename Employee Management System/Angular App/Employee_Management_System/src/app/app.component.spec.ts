2996d26eb2276bb2e55f43ab591f72b88140bf50
Aman Shrivastava <amann.shrivastava@gmail.com>
2024-04-30 11:11:21 +0530

    New no standalone template

--- Employee Management System/Angular App/Employee_Management_System/src/app/app.component.spec.ts
+++ Employee Management System/Angular App/Employee_Management_System/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Employee_Management_System' title`, () => {
+  it(`should have as title 'Employee_Management_System'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Employee_Management_System');
