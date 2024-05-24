263f69f7aa14b17c0a308729ef8c5065321ef30a
Aman Shrivastava <amann.shrivastava@gmail.com>
2024-05-24 10:52:25 +0530

    Dashboard Layout

--- Employee Management System/angular_app/src/app/app-routing.module.ts
+++ Employee Management System/angular_app/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { NavbarComponent } from './landing-page/navbar/navbar.component';
+import { HomePageComponent } from './landing-page/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
